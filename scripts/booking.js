4a0134f56db36fe1e25eb195599461455a0be9bf
Zalia <zaliarath@gmail.com>
2026-04-15 22:07:11 -0600

    good enough

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,11 +3,13 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
-let dayNum = 0;
-let halfNum = 0;
-let dayCost = (dayNum * 35) + (halfNum * 20);
 let day = document.querySelectorAll("#monday, #tuesday, #wednesday, #thursday, #friday");
 let clearBtn = document.querySelector("#clear-button");
+let halfBtn = document.querySelector("#half");
+let fullBtn = document.querySelector("#full");
+let dailyRate = 35;
+let dayCounter = 0;
+let totalCost = 0;
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
@@ -16,7 +18,7 @@ let clearBtn = document.querySelector("#clear-button");
 day.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.classList.add("clicked");
-    dayNum += 1;
+    dayCounter ++;
     });
 });
 
@@ -25,23 +27,30 @@ day.forEach((btn) => {
 
 clearBtn.addEventListener("click", () => {
   day.forEach((d) => d.classList.remove("clicked"));
-  dayNum = 0;
+  dayCounter = 0;
 });
-
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
-
-
+halfBtn.addEventListener("click", () => {
+  halfBtn.classList.add("clicked");
+  fullBtn.classList.remove("clicked");
+  dailyRate = 20;
+});
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
-
-
-
+fullBtn.addEventListener("click", () => {
+  fullBtn.classList.add("clicked");
+  halfBtn.classList.remove("clicked");
+  dailyRate = 35;
+});
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
-
+while (dayCounter > 0) {
+  let totalCost = dailyRate * dayCounter; 
+  document.querySelector("#calculated-cost").innerHTML = totalCost;
+}
